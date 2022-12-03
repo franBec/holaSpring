64fbf701f5c6443bcdca4e12394597738cb63750
Fran Bec <franbecvort@gmail.com>
2022-12-03 20:36:32 -0300

    [S2C10] Ejercicio - HolaMundo con Thymeleaf y Spring MVC

--- src/main/java/com/example/holaspring/ControladorInicio.java
+++ src/main/java/com/example/holaspring/ControladorInicio.java
@@ -1,16 +1,15 @@
 package com.example.holaspring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Slf4j
 public class ControladorInicio {
 
     @GetMapping("/")
     public String inicio(){
-        log.info("esto es un log");
-        return "Hola mundo con Spring";
+        return "index";
     }
 }
