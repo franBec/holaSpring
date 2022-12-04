c2cc7772961cd75cdc40145e5ff13f52193ab32b
Fran Bec <franbecvort@gmail.com>
2022-12-03 21:38:23 -0300

    [S2C13] Creación de una lista de Personas

--- src/main/java/com/example/holaspring/ControladorInicio.java
+++ src/main/java/com/example/holaspring/ControladorInicio.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 @Slf4j
 public class ControladorInicio {
@@ -22,9 +24,15 @@ public class ControladorInicio {
         persona.setEmail("juan@gmail.com");
         persona.setTelefono("123");
 
+        var persona2 = new Persona();
+        persona2.setNombre("Carla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("carla@gmail.com");
+        persona2.setTelefono("999");
+
         model.addAttribute("mensaje", "Mensaje con thymeleaf");
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        model.addAttribute("personaList", Arrays.asList(persona, persona2));
 
         return "index";
     }
