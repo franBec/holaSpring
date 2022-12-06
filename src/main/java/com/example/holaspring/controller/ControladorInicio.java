9e7d8421db85d115c34157e4e452f8dcf78649f9
Fran Bec <franbecvort@gmail.com>
2022-12-05 23:09:14 -0300

    [S5C21] Creación de un CRUD con Spring Boot y MySql - Agregar

--- src/main/java/com/example/holaspring/controller/ControladorInicio.java
+++ src/main/java/com/example/holaspring/controller/ControladorInicio.java
@@ -1,11 +1,13 @@
 package com.example.holaspring.controller;
 
+import com.example.holaspring.domain.Persona;
 import com.example.holaspring.service.PersonaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -18,5 +20,16 @@ public class ControladorInicio {
     public String inicio(Model model){
         model.addAttribute("personaList", personaService.findAll());
         return "index";
+    }
+
+    @GetMapping("/create")
+    public String create(Persona persona){
+        return "edit";
+    }
+
+    @PostMapping("/save")
+    public String save(Persona persona){
+        personaService.save(persona);
+        return "redirect:/";
     }
 }
