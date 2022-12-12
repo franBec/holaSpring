557744f842d2336b5005b733d0ed02cf057b11b8
Fran Bec <franbecvort@gmail.com>
2022-12-11 21:37:07 -0300

    [S9C34] Seguridad con Spring y Hibernate/JPA - parte 4

--- src/main/java/com/example/holaspring/config/WebConfig.java
+++ src/main/java/com/example/holaspring/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.holaspring.controller;
+package com.example.holaspring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
